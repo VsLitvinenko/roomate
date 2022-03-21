406a953af3cdf1f635993566edd53a006de734c6
VLitvinenko <VLitvinenko@bellintegrator.com>
2022-03-22 01:04:18 +0300

    feature/theming: toogle dark mode demo

--- src/app/pages/channel/pages/channel-chat/components/channel-header/channel-header.component.ts
+++ src/app/pages/channel/pages/channel-chat/components/channel-header/channel-header.component.ts
@@ -7,9 +7,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ChannelHeaderComponent implements OnInit {
   @Input() public title: string;
+  private darkMode = window?.matchMedia('(prefers-color-scheme: dark)').matches;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.toggleDarkMode(this.darkMode);
+    this.initPrefersDark();
+  }
+
+  public changeDarkMode(): void {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+
+  private initPrefersDark(): void {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    prefersDark.addEventListener('change', e => {
+      if (this.darkMode !== e.matches) {
+        this.darkMode = !this.darkMode;
+        this.toggleDarkMode(this.darkMode);
+      }
+    });
+  }
+
+  private toggleDarkMode(enable): void {
+    document.body.classList.toggle('dark', enable);
+  }
 
 }
