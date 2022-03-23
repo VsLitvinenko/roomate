db8d94ea0535456479630493a798d89d148f096d
VLitvinenko <VLitvinenko@bellintegrator.com>
2022-03-23 23:47:25 +0300

    feature/theming: colors restyle

--- src/app/pages/channel/components/side-menu/side-menu.component.ts
+++ src/app/pages/channel/components/side-menu/side-menu.component.ts
@@ -9,15 +9,15 @@ import {Router} from '@angular/router';
 export class SideMenuComponent implements OnInit {
 
   publicChannels = [
-    { title: 'public 1', id: 1 },
-    { title: 'public 2', id: 2 },
-    { title: 'public 3', id: 3 },
+    { title: 'public 1', id: 1, unread: false },
+    { title: 'public 2', id: 2, unread: true },
+    { title: 'public 3', id: 3, unread: false },
   ];
   privateChannels = [
-    { title: 'private 1', id: 1 },
-    { title: 'private 2', id: 2 },
-    { title: 'private 3', id: 3 },
-    { title: 'private 4', id: 4 },
+    { title: 'private 1', id: 1, unread: false },
+    { title: 'private 2', id: 2, unread: false },
+    { title: 'private 3', id: 3, unread: true },
+    { title: 'private 4', id: 4, unread: true },
   ];
 
   constructor(private readonly router: Router) { }
