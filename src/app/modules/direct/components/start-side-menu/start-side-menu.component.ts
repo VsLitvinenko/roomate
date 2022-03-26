e44efa1e5cd8e4a417908fe5ae90a581fbad3f2f
VLitvinenko <VLitvinenko@bellintegrator.com>
2022-03-27 02:11:59 +0300

    feature/direct: start-side-menu demo style

--- src/app/modules/direct/components/start-side-menu/start-side-menu.component.ts
+++ src/app/modules/direct/components/start-side-menu/start-side-menu.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { usersList } from './data-source';
+import { splitPaneBreakPoint } from '../../../shared/constants';
 
 @Component({
   selector: 'app-start-side-menu',
@@ -8,8 +10,24 @@ import {Router} from '@angular/router';
 })
 export class StartSideMenuComponent implements OnInit {
 
+  public userList = usersList;
+  public msgMaxWidth: number;
+
   constructor(private readonly router: Router) { }
 
-  ngOnInit() {
+  @HostListener('window:resize', ['$event'])
+  onAppResize(event: any = null): void {
+    const appWidth = event === null ? window.innerWidth : event.target.innerWidth;
+    // pc menu max width = 348 - x = 242; x = 106 (with overflow)
+    // so mobile menu max width = 304 - 106 = 198
+    this.msgMaxWidth = appWidth > splitPaneBreakPoint.minWidth ? 242 : 198;
+  }
+
+  ngOnInit(): void {
+    this.onAppResize();
+  }
+
+  public openCurrentDirect(id: string) {
+    this.router.navigate(['direct', 'current', id]).then();
   }
 }
