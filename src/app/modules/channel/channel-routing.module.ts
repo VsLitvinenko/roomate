278cc87cc99afc5331638b373cd0f98d5b39057d
VLitvinenko <VLitvinenko@bellintegrator.com>
2022-03-27 17:40:11 +0300

    feature/app: random fixes

--- src/app/modules/channel/channel-routing.module.ts
+++ src/app/modules/channel/channel-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChannelPage } from './view/channel.page';
-import {CurrentChannelComponent} from './pages/current-channel/current-channel.component';
-import {NoChannelComponent} from './pages/no-channel/no-channel.component';
+import { CurrentChannelComponent } from './pages/current-channel/current-channel.component';
+import { NoChannelComponent } from './pages/no-channel/no-channel.component';
 
 const routes: Routes = [
   {
