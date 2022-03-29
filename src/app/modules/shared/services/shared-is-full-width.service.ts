2b5275ea915c43e4b56ffbfeb30dacd56ce5e213
VLitvinenko <VLitvinenko@bellintegrator.com>
2022-03-29 21:39:56 +0300

    feature/app: make isFullApp$ hot, random fixes

--- src/app/modules/shared/services/shared-is-full-width.service.ts
+++ src/app/modules/shared/services/shared-is-full-width.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { fromEvent, Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import {map, shareReplay, startWith} from 'rxjs/operators';
 import { splitPaneBreakPoint } from '../constants';
 
 const minWidth = splitPaneBreakPoint.minWidth;
@@ -13,7 +13,8 @@ export class SharedIsFullWidthService {
   public readonly isAppFullWidth$: Observable<boolean> = fromEvent(window, 'resize')
     .pipe(
       map((event: any) => event.target.innerWidth > minWidth ),
-      startWith(window.innerWidth > minWidth)
+      startWith(window.innerWidth > minWidth),
+      shareReplay(1),
     );
 
   constructor() {
