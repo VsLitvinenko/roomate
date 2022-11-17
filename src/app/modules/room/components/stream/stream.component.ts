7ebd09e9d3292b15a8e0c2247e0d1a997f039497
VsLitvinenko <alorsl@live.com>
2022-11-17 07:55:19 +0300

    feature/stream: audio level border + local/remote id fix

--- src/app/modules/room/components/stream/stream.component.ts
+++ src/app/modules/room/components/stream/stream.component.ts
@@ -23,20 +23,32 @@ export class StreamComponent implements OnChanges, AfterViewInit, OnDestroy {
   @Input() public videoTrack: MediaStreamTrack;
   @Input() public audioTrack: MediaStreamTrack;
   @Input() public index: number;
+  @Input() public localStream: boolean;
 
+  @ViewChild('speakerBorder') private readonly speakerBorder: ElementRef<HTMLElement>;
   @ViewChild('videoEl') private readonly videoEl: ElementRef<HTMLVideoElement>;
   @ViewChild('audioEl') private readonly audioEl: ElementRef<HTMLAudioElement>;
   @ViewChild('modal') private readonly modal: IonModal;
   @ViewChild('popover') private readonly popover: IonPopover;
 
-  public audioVolume = 100;
+  public audioVolume: number;
   public modalLoading = false;
 
   private remoteVideoStream: MediaStream;
   private remoteAudioStream: MediaStream;
   private playerReady$ = new BehaviorSubject(false);
 
+  private vumeterNode: AudioWorkletNode;
+
   constructor() { }
+
+  public get popoverId(): string {
+    return `popover-${this.localStream ? 'local' : 'remote'}${this.index}`;
+  }
+
+  public get modalVideoElId(): string {
+    return `modal-video-el-${this.localStream ? 'local' : 'remote'}${this.index}`;
+  }
 
   private get playerReady(): Observable<void> {
     return this.playerReady$.pipe(
@@ -47,6 +59,10 @@ export class StreamComponent implements OnChanges, AfterViewInit, OnDestroy {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (changes.localStream) {
+      this.audioVolume = this.localStream ? 0 : 100;
+    }
+    // set tracks only after view init
     this.playerReady.subscribe(() => {
       if (changes.videoTrack) {
         this.trackHandler(this.videoTrack, 'video');
@@ -66,6 +82,10 @@ export class StreamComponent implements OnChanges, AfterViewInit, OnDestroy {
     await this.popover.dismiss();
   }
 
+  public setVolume(volume: number): void {
+    this.audioVolume = volume;
+  }
+
   public openPopover(): void {
     this.popover.present().then();
   }
@@ -74,8 +94,7 @@ export class StreamComponent implements OnChanges, AfterViewInit, OnDestroy {
     this.modalLoading = true;
     this.modal.present()
       .then(() => {
-        const modalVideoEl = document
-          .getElementById(`modal-video-el-${this.index}`) as HTMLVideoElement;
+        const modalVideoEl = document.getElementById(this.modalVideoElId) as HTMLVideoElement;
         this.videoEl.nativeElement.srcObject = null;
 
         Janus.attachMediaStream(modalVideoEl, this.remoteVideoStream);
@@ -105,8 +124,50 @@ export class StreamComponent implements OnChanges, AfterViewInit, OnDestroy {
       }
       else {
         this.remoteAudioStream = stream;
+        this.audioTrackLevel().then();
       }
     }
     setTimeout(() => Janus.attachMediaStream(attachEl, stream), 0);
+  }
+
+  private async audioTrackLevel(): Promise<void> {
+    const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
+    const microphone = audioContext.createMediaStreamSource(this.remoteAudioStream);
+    const node = await this.getVumeterNode(audioContext);
+    this.speakerBorderControl(node);
+    microphone.connect(node).connect(audioContext.destination);
+  }
+
+  private async getVumeterNode(context: AudioContext): Promise<AudioWorkletNode> {
+    if (!this.vumeterNode) {
+      await context.audioWorklet.addModule('/assets/vumeter/vumeter-processor.js');
+      this.vumeterNode = new AudioWorkletNode(context, 'vumeter');
+    }
+    return this.vumeterNode;
+  }
+
+  private speakerBorderControl(node: AudioWorkletNode) {
+    const maxCounter = 15;
+    let lastMaxRes = 0;
+    let counter = 0;
+
+    // updateInterval is 100ms
+    node.port.onmessage = event => {
+      if (!event.data.volume) {
+        return;
+      }
+      const res = Math.round(
+        event.data.volume * 1000 / 2.5
+      ) / 100;
+      if (res > lastMaxRes || counter === maxCounter) {
+        // max value during last 1.5 sec
+        lastMaxRes = res;
+        this.speakerBorder.nativeElement.style.opacity = res.toString();
+      }
+      counter += 1;
+      if (counter > maxCounter) {
+        counter = 0;
+      }
+    };
   }
 }
