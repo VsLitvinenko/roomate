83b62fe059039f5d17b8e2228d8828f083a16b01
VsLitvinenko <alorsl@live.com>
2022-12-02 20:36:17 +0300

    feature/main: shorten imports for common main

---
+++ src/app/main/components/index.ts
@@ -0,0 +1,2 @@
+export * from './profile-header/profile-header.component';
+export * from './tabs/tabs.component';
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,8 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { MainComponent } from './view/main.component';
 import { MainRoutingModule } from './main-routing.module';
 import { CommonModule } from '@angular/common';
-import { TabsComponent } from './components/tabs/tabs.component';
-import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
+import { TabsComponent, ProfileHeaderComponent } from './components';
 import { MenuControllerService } from './services/menu-controller.service';
 
 @NgModule({
