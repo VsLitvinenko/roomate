14873430501ac617599d5fc803f57e3f205770ee
VsLitvinenko <alorsl@live.com>
2023-02-18 16:10:13 +0300

    feature/channel: create channel modal styling

--- src/app/main/pages/channel/components/create-channel-modal/create-channel-modal.component.ts
+++ src/app/main/pages/channel/components/create-channel-modal/create-channel-modal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { UsersService } from '../../../../../core';
+import { BehaviorSubject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-create-channel-modal',
@@ -7,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateChannelModalComponent implements OnInit {
 
-  constructor() { }
+  public readonly searchUserList$ = this.users.getUsersList([1, 2, 3, 4, 5, 6]);
+
+  public readonly membersIds$ = new BehaviorSubject<number[]>([this.users.selfId]);
+
+  public readonly members$ = this.membersIds$.pipe(
+    switchMap(ids => this.users.getUsersList(ids))
+  );
+
+  constructor(private readonly modalCtrl: ModalController,
+              private readonly users: UsersService) { }
 
   ngOnInit(): void {}
+
+  public async closeModal(): Promise<void> {
+    await this.modalCtrl.dismiss();
+  }
 
 }
