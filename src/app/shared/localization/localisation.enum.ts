d3ba6ba3992da8e8776b14d8879a7d7b83d61093
VsLitvinenko <alorsl@live.com>
2023-02-20 14:24:12 +0300

    feature/localization: create new channel modal localisation

--- src/app/shared/localization/localisation.enum.ts
+++ src/app/shared/localization/localisation.enum.ts
@@ -1,3 +1,8 @@
+interface LocalisationLanguages {
+  en: string;
+  ru: string;
+}
+
 export enum LocalisationEnum {
   unknownError = 'unknownError',
   noMessages = 'noMessages',
@@ -5,36 +10,61 @@ export enum LocalisationEnum {
   directs = 'directs',
   room = 'room',
   settings = 'settings',
-}
-
-interface LocalisationLanguages {
-  en: string;
-  // ru: string;
+  noUsers = 'noUsers',
+  close = 'close',
+  create = 'create',
+  publicChannel = 'publicChannel',
+  channelsName = 'channelsName',
+  createNewChannel = 'createNewChannel',
 }
 
 export const localisationValues = new Map<keyof typeof LocalisationEnum, LocalisationLanguages>([
   [
     LocalisationEnum.unknownError,
-    { en: 'UNKNOWN ERROR' }
+    { en: 'UNKNOWN ERROR', ru: 'НЕИЗВЕСТНАЯ ОШИБКА' }
   ],
   [
     LocalisationEnum.noMessages,
-    { en: 'No messages' }
+    { en: 'No messages', ru: 'Нет сообщений' }
   ],
   [
     LocalisationEnum.channels,
-    { en: 'Channels' }
+    { en: 'Channels', ru: 'Каналы' }
   ],
   [
     LocalisationEnum.directs,
-    { en: 'Directs' }
+    { en: 'Directs', ru: 'Сообщения' }
   ],
   [
     LocalisationEnum.room,
-    { en: 'Room' }
+    { en: 'Room', ru: 'Комната' }
   ],
   [
     LocalisationEnum.settings,
-    { en: 'Settings' }
+    { en: 'Settings', ru: 'Настройки' }
+  ],
+  [
+    LocalisationEnum.noUsers,
+    { en: 'No users', ru: 'Нет пользователей' }
+  ],
+  [
+    LocalisationEnum.close,
+    { en: 'Close', ru: 'Закрыть' }
+  ],
+  [
+    LocalisationEnum.create,
+    { en: 'Create', ru: 'Создать' }
+  ],
+  [
+    LocalisationEnum.publicChannel,
+    { en: 'Public channel', ru: 'Публичный канал' }
+  ],
+  [
+    LocalisationEnum.channelsName,
+    { en: 'Channel\'s name', ru: 'Название канала' }
+  ],
+  [
+    LocalisationEnum.createNewChannel,
+    { en: 'Create new channel', ru: 'Создать новый канал' }
   ],
 ]);
