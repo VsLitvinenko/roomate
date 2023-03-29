50dcbee3c0d069e931b54e00dbe464d7bbd82dca
thin-tony <ant.larionow@gmail.com>
2023-03-29 20:16:00 +0300

    Some changes for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  signalR: 'http://localhost:8100',
-  janus: 'http://localhost:8088'
+  signalR: 'https://dev.roomate.ru/api',
+  janus: 'https://dev.roomate.ru/janus-gateway'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  signalR: 'http://localhost:8100',
-  janus: 'http://localhost:8088'
+  signalR: 'https://dev.roomate.ru/api',
+  janus: 'https://dev.roomate.ru/janus-gateway'
 };
 
 /*
