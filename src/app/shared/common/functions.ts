a87657d8682c536f413e372171beec6ba4668563
VsLitvinenko <alorsl@live.com>
2023-04-02 17:36:08 +0300

    feature/common: update last read message [2] get visible messages on scroll

--- src/app/shared/common/functions.ts
+++ src/app/shared/common/functions.ts
@@ -15,3 +15,25 @@ export const extractError = (error: any): string => {
     return 'UNKNOWN ERROR';
   }
 };
+
+export const filterVisibleElements = (array: HTMLElement[], scrollTarget: HTMLElement) => array.filter(item => {
+  const rect = item.getBoundingClientRect();
+  return ((rect.y + rect.height) > 0 && ((rect.bottom - rect.height) < scrollTarget.clientHeight));
+});
+
+export const openElementsChildren = (
+  array: HTMLElement[],
+  options: {
+    filterHandler?: (...args: any[]) => boolean;
+    reverseLeaf?: boolean;
+  } = {}) =>
+  array.reduce((previousValue, currentValue) => {
+    let leaf = Array.from(currentValue.children);
+    if (options.filterHandler) {
+      leaf = leaf.filter(options.filterHandler);
+    }
+    if (options.reverseLeaf) {
+      leaf.reverse();
+    }
+    return [...previousValue, ...leaf] as HTMLElement[];
+  }, []);
